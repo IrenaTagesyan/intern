18b213cd14431369dd1d91a435f08f73d07d6e18
IrenaTagesyan <109281677+IrenaTagesyan@users.noreply.github.com>
2023-05-07 12:38:09 +0400

    Update Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,24 +7,50 @@ function Nav() {
       <ul>
         <li>
           <NavLink className="link" to="/" activeClassName="active">
+            {({ isActive, isPending }) => (
+              <span
+                className={isPending ? "pending" : isActive ? "active" : ""}
+              >
             Home
+              </span>
+            )}
           </NavLink>
         </li>
 
         <li>
           <NavLink className="link" to="/shop" activeClassName="active">
+            
+{({ isActive, isPending }) => (
+              <span
+                className={isPending ? "pending" : isActive ? "active" : ""}
+              >
             Shop
+              </span>
+            )}
           </NavLink>
         </li>
 
         <li>
           <NavLink className="link" to="/aboutus" activeClassName="active">
+            
+            {({ isActive, isPending }) => (
+              <span
+                className={isPending ? "pending" : isActive ? "active" : ""}
+              >
             About us
+              </span>
+            )}
           </NavLink>
         </li>
         <li>
           <NavLink className="link" to="/contact" activeClassName="active">
+            {({ isActive, isPending }) => (
+              <span
+                className={isPending ? "pending" : isActive ? "active" : ""}
+              >
             Contact us
+              </span>
+            )}
           </NavLink>
         </li>
       </ul>
